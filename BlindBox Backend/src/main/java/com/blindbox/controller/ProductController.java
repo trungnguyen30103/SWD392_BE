d8fda33a1645faa64e7623078b7a71e70449233a
HMcPhong <167400827+HMcPhong@users.noreply.github.com>
2025-03-17 12:45:46 +0700

    Update using new swagger

--- BlindBox Backend/src/main/java/com/blindbox/controller/ProductController.java
+++ BlindBox Backend/src/main/java/com/blindbox/controller/ProductController.java
@@ -2,9 +2,9 @@ package com.blindbox.controller;
 
 import com.blindbox.model.Product;
 import com.blindbox.service.ProductService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Api(value = "Product Management System", description = "Operations pertaining to products in the Product Management System")
+@Tag(name = "Product Management System", description = "Operations pertaining to products in the Product Management System")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -20,16 +20,16 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @ApiOperation(value = "Get all products", response = List.class)
+    @Operation(summary = "Get all products", description = "Retrieve a list of all available products")
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "Get a product by ID", response = Product.class)
+    @Operation(summary = "Get a product by ID", description = "Retrieve a single product using its ID")
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@ApiParam(value = "ID of the product to be fetched", required = true) @PathVariable Integer id) {
+    public ResponseEntity<Product> getProductById(@Parameter(description = "ID of the product to retrieve", required = true) @PathVariable Integer id) {
         Product product = productService.getProductById(id);
         if (product != null) {
             return new ResponseEntity<>(product, HttpStatus.OK);
@@ -38,17 +38,17 @@ public class ProductController {
         }
     }
 
-    @ApiOperation(value = "Create a new product", response = Product.class)
+    @Operation(summary = "Create a new product", description = "Add a new product to the catalog")
     @PostMapping
-    public ResponseEntity<Product> createProduct(@ApiParam(value = "Product object to be created", required = true) @RequestBody Product product) {
+    public ResponseEntity<Product> createProduct(@Parameter(description = "Product object to be created", required = true) @RequestBody Product product) {
         Product createdProduct = productService.createProduct(product);
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
 
-    @ApiOperation(value = "Update an existing product", response = Product.class)
+    @Operation(summary = "Update an existing product", description = "Update an existing product using its ID")
     @PutMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@ApiParam(value = "ID of the product to be updated", required = true) @PathVariable Integer id,
-                                                 @ApiParam(value = "Updated product object", required = true) @RequestBody Product product) {
+    public ResponseEntity<Product> updateProduct(@Parameter(description = "ID of the product to be updated", required = true) @PathVariable Integer id,
+                                                 @Parameter(description = "Updated product object", required = true) @RequestBody Product product) {
         Product updatedProduct = productService.updateProduct(id, product);
         if (updatedProduct != null) {
             return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
@@ -57,9 +57,9 @@ public class ProductController {
         }
     }
 
-    @ApiOperation(value = "Delete a product by ID")
+    @Operation(summary = "Delete a product by ID")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@ApiParam(value = "ID of the product to be deleted", required = true) @PathVariable Integer id) {
+    public ResponseEntity<Void> deleteProduct(@Parameter(description = "ID of the product to be deleted", required = true) @PathVariable Integer id) {
         if (productService.deleteProduct(id)) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
